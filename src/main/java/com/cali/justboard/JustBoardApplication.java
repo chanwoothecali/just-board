790cc3fc708c4c431df0fcf7ca4fc787427e21e0
macCali <jungcali94@gmail.com>
2022-07-23 15:30:56 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기회
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    *롬복

---
+++ src/main/java/com/cali/justboard/JustBoardApplication.java
@@ -0,0 +1,13 @@
+package com.cali.justboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustBoardApplication.class, args);
+	}
+
+}
