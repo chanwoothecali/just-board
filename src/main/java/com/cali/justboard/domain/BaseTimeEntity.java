a9d1bd7aadd0e014219b227bbd3f3fb430e1d3b4
macCali <jungcali94@gmail.com>
2022-08-20 14:19:40 +0900

    #12 - BaseEntity 추출
    생성자, 생성일시, 수정자, 수정일시는 중복 요소라 @MappedSuperclass를 이용하여 상속 추출함

---
+++ src/main/java/com/cali/justboard/domain/BaseTimeEntity.java
@@ -0,0 +1,31 @@
+package com.cali.justboard.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+}
