f39d748deb2a8ac334b111522e71cee0f538516e
macCali <jungcali94@gmail.com>
2022-08-21 15:15:30 +0900

    #1 - Querydsl 지원을 이용해 api 검색옵션 추가
    QuerydslPredicateExecutor
    QuerydslBinderCustomizer

--- src/main/java/com/cali/justboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/cali/justboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,28 @@
 package com.cali.justboard.repository;
 
 import com.cali.justboard.domain.ArticleComment;
+import com.cali.justboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        bindings.excludeUnlistedProperties(true); // 리스팅 하지 않은 결과는 검색에서 제외
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // 시분초까지 동일하게 검색해야 해서 좋은 조건은 아님. 추후 수정 필요
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
