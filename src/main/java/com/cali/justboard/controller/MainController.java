bf4c1c5d6eaea9d4f3f1d8382b639d64968aea0e
macCali <jungcali94@gmail.com>
2022-09-01 20:57:23 +0900

    #19 - root 경로를 게시판으로 forward하도록 컨트롤러 생성
    root 경로를 게시판 페이지로 설정한다.

--- src/main/java/com/cali/justboard/controller/MainController.java
+++ src/main/java/com/cali/justboard/controller/MainController.java
@@ -1,2 +1,13 @@
-package com.cali.justboard.controller;public class MainController {
+package com.cali.justboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "forward:/articles";
+    }
 }
